488bbeaa16a57e62f7cad6d3306536fd58d56776
Harsh Singh <kmrharsh50@gmail.com>
2021-11-12 23:56:40 +0530

    [Harsh]Added:Fibonacci Series Logical-Problem Features Implemented

---
+++ Logical-problems/Fibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical_problems
+{
+    class Fibonacci
+    {
+        public void FibonacciS()
+        {
+            int first = 0, second = 1, third = 0;
+            Console.WriteLine("Enter the number of series :");
+            int series = Convert.ToInt32(Console.ReadLine());
+            if (series <= 1)
+            {
+                Console.WriteLine(first);
+                return;
+            }
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for (int i = 2; i < series; i++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+                Console.WriteLine(third);
+            }
+        }
+    }
+}
---
+++ Logical-problems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Logical_problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Problems \n ");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Fibonacci series = new Fibonacci();
+                        series.FibonacciS();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
