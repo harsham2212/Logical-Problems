6dae37fc420ed1bbc815edb256558aea93cb591e
Harsh Singh <kmrharsh50@gmail.com>
2021-11-13 17:01:32 +0530

    [Harsh]Added:Coupon Number Logical-Problem Features Implemented

--- Logical-problems/Program.cs
+++ Logical-problems/Program.cs
@@ -10,31 +10,33 @@ namespace Logical_problems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse Number \n 5. Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect Number \n 3. Prime Number \n 4. Reverse Number \n 5. Coupon Number \n 6. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        Console.Write("Enter the number :");
                         Fibonacci series = new Fibonacci();
                         series.FibonacciS();
                         break;
                     case 2:
-                        Console.Write("Enter the number :");
                         PerfectNumber number = new PerfectNumber();
                         number.Perfect();
                         break;
                     case 3:
-                        Console.Write("Enter the number :");
                         PrimeNumber num = new PrimeNumber();
                         num.Prime();
                         break;
                     case 4:
-                        Console.Write("Enter the number :");
                         ReverseNumber rev = new ReverseNumber();
                         rev.Reverse();
                         break;
                     case 5:
+                        Console.Write("How many numbers you wants to generate :");
+                        int N = Convert.ToInt32(Console.ReadLine());
+                        CouponNumber cn = new CouponNumber();
+                        cn.Calculation(N);
+                        break;
+                    case 6:
                         flag = false;
                         break;
                 }
