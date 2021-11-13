a9af4ef5d80fef4e666e423906e7f660a84d6170
Harsh Singh <kmrharsh50@gmail.com>
2021-11-13 16:34:47 +0530

    [Harsh]Added:Perfect Number Logical-Problem Features Implemented

--- Logical-problems/Program.cs
+++ Logical-problems/Program.cs
@@ -10,15 +10,21 @@ namespace Logical_problems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2.Exit");
+                Console.WriteLine("Enter the program number which is to be executed \n 1. Fibonacci Series \n 2. Perfect Number \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
+                        Console.Write("Enter the number :");
                         Fibonacci series = new Fibonacci();
                         series.FibonacciS();
                         break;
                     case 2:
+                        Console.Write("Enter the number :");
+                        PerfectNumber number = new PerfectNumber();
+                        number.Perfect();
+                        break; ;
+                    case 3:
                         flag = false;
                         break;
                 }
